6428372f2b4c473057f02d1917bc70416672bffa
covacode <covascode@gmail.com>
2020-07-04 15:24:04 -0300

    Commit Inicial

---
+++ js/scrolling.js
@@ -0,0 +1,70 @@
+
+$('#Home').on("click", function () {
+    var dest = $("#page-top").offset().top;
+    $('html, body').animate({scrollTop: dest}, 800);
+});
+
+$('#Home_Phone').on("click", function () {
+    var dest = $("#page-top").offset().top;
+    $('html, body').animate({scrollTop: dest}, 800);
+});
+
+$('#About').on("click", function () {
+    var dest = $("#about").offset().top;
+    $('html, body').animate({scrollTop: dest}, 800);
+});
+
+$('#About_Phone').on("click", function () {
+    var dest = $("#about").offset().top;
+    $('html, body').animate({scrollTop: dest}, 800);
+});
+
+$('#Curriculum').on("click", function () {
+    var dest = $("#curriculum").offset().top;
+    $('html, body').animate({scrollTop: dest}, 800);
+});
+
+$('#Curriculum_Phone').on("click", function () {
+    var dest = $("#curriculum").offset().top;
+    $('html, body').animate({scrollTop: dest}, 800);
+});
+
+$('#Programs').on("click", function () {
+    var dest = $("#programs").offset().top;
+    $('html, body').animate({scrollTop: dest}, 800);
+});
+
+$('#Programs_Phone').on("click", function () {
+    var dest = $("#programs").offset().top;
+    $('html, body').animate({scrollTop: dest}, 800);
+});
+
+$('#Staff').on("click", function () {
+    var dest = $("#staff").offset().top;
+    $('html, body').animate({scrollTop: dest}, 800);
+});
+
+$('#Staff_Phone').on("click", function () {
+    var dest = $("#staff").offset().top;
+    $('html, body').animate({scrollTop: dest}, 800);
+});
+
+$('#Gallery').on("click", function () {
+    var dest = $("#gallery").offset().top;
+    $('html, body').animate({scrollTop: dest}, 800);
+});
+
+$('#Gallery_Phone').on("click", function () {
+    var dest = $("#gallery").offset().top;
+    $('html, body').animate({scrollTop: dest}, 800);
+});
+
+$('#Contact').on("click", function () {
+    var dest = $("#contact").offset().top;
+    $('html, body').animate({scrollTop: dest}, 800);
+});
+
+$('#Contact_Phone').on("click", function () {
+    var dest = $("#contact").offset().top;
+    $('html, body').animate({scrollTop: dest}, 800);
+});
